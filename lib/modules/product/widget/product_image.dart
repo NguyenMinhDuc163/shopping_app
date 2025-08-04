6691ad8d01a480a3af8c6acf9a47ed26c59cc721
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2025-08-05 00:02:02 +0700

    [feat]: implement custom paint circle widget with configurable image overlay

--- lib/modules/product/widget/product_image.dart
+++ lib/modules/product/widget/product_image.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopping_app/core/constants/icon_path.dart';
 import 'package:shopping_app/core/constants/image_path.dart';
 import 'package:shopping_app/core/theme/app_colors.dart';
+import 'package:shopping_app/modules/product/widget/circle_widget.dart';
 
 class ProductImage extends StatelessWidget {
   const ProductImage({super.key, this.stackChildren = const []});
@@ -17,27 +18,41 @@ class ProductImage extends StatelessWidget {
           fit: BoxFit.cover,
         ),
         Positioned(
-          bottom: -24,
+          bottom: -48,
           left: 0,
           right: 0,
-          child: Center(
-            child: Container(
+          child: SizedBox(
               width: 80,
-              height: 80,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: AppColors.colorD9E3DC,
-                shape: BoxShape.circle,
-              ),
-              child: SvgPicture.asset(
-                IconPath.iconNike,
-                width: 30,
-                height: 30,
-              ),
-            ),
-          ),
+              height: 110,
+              child: CircleWidget(imagePath: IconPath.iconNike,)),
         ),
-        ...stackChildren
+
+        // Positioned(
+        //   bottom: -30,
+        //   left: 0,
+        //   right: 0,
+        //   child: Center(
+        //     child: ClipOval(
+        //       child: BackdropFilter(
+        //         filter: ImageFilter.blur(sigmaX: 60, sigmaY: 60),
+        //         child: Container(
+        //           width: 80,
+        //           height: 80,
+        //           alignment: Alignment.center,
+        //           child: Transform.translate(
+        //             offset: const Offset(0, -8),
+        //             child: SvgPicture.asset(
+        //               IconPath.iconNike,
+        //               width: 30,
+        //               height: 30,
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        ...stackChildren,
       ],
     );
   }
