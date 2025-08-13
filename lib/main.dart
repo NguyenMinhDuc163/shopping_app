69d7f1fa29835a064e1da2dbd5fe9e733910f3d4
Nguyen Duc <DucNM.B21CN249@stu.ptit.edu.vn>
2025-08-13 23:13:55 +0700

    [feat] setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/adapters.dart';
+import 'package:shopping_app/utils/helpers/local_storage_helper.dart';
 
+import 'firebase_options.dart';
 import 'modules/app.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Hive.initFlutter();
+  await LocalStorageHelper.initLocalStorageHelper();
+  await EasyLocalization.ensureInitialized();
 
-void main() {
-  runApp(const App());
+  String? savedLocale = await LocalStorageHelper.getValue('languageCode');
+  Locale defaultLocale = const Locale('en', 'US');
+  if (savedLocale != null) {
+    if (savedLocale == 'vi') {
+      defaultLocale = const Locale('vi', 'VN');
+    } else {
+      defaultLocale = const Locale('en', 'US');
+    }
+  }
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en', 'US'), Locale('vi', 'VN')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      startLocale: defaultLocale,
+      child: const App(),
+    ),
+  );
 }
